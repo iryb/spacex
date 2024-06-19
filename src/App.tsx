8ec9876adfef12f6b3508de30eba6f8c0e18c554
Iryna <iryna.burianovata@gmail.com>
2024-06-19 15:00:14 +0300

    Fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
+import { ApolloProvider } from "@apollo/client";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/pages/Home";
+import { client } from "./lib/data";
 
 export default function App() {
   return (
     <main>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </ApolloProvider>
     </main>
   );
 }
